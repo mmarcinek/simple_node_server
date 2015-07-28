08eb40edca14c1f98b0f1fd02a5ec4ea38027829
Michael Marcinek <michaelmarcinek@gmail.com>
2015-07-27 22:54:47 -0400

    Adds Mongoose and Body-Parser

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-var http = require('http');
+var express = require('express');
 
-var server = http.createServer( function (req, res){
-  res.writeHead(200);
-  res.write('<p>Hello World</p>');
-  res.end();
-});
+var server = express();
+server.use(express.static(__dirname + '/public'));
 
-var port = 8053;
-server.listen(port, function() {
-  console.log('server is listening on port' + port);
+var port = 8023;
+server.listen(port, function () {
+  console.log('server is litening on port' + port);
 });
