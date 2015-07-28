c7909a8904a33675227583ec670274bbf475a5de
Michael Marcinek <michaelmarcinek@gmail.com>
2015-07-27 23:09:58 -0400

    Creates simple server and configs a test route: Ping GET from postman returns test JSON... Hooray!!!

--- server.js
+++ server.js
@@ -1,9 +1,44 @@
-var express = require('express');
+// server.js
 
-var server = express();
-server.use(express.static(__dirname + '/public'));
+//Base SETUP
+// =============================================================================
 
-var port = 8023;
-server.listen(port, function () {
-  console.log('server is litening on port' + port);
+// require the node packages
+
+var express    = require('express');      // call express
+var app        = express();               // define our app using express
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will get the data from a POST req
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;      // sets our PORT
+
+// Routes for our API
+// ==============================================================================
+
+var router = express.Router();            // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+
+router.get('/', function(req, res) {
+  res.json({ message: 'Hooray! Welcome to your first API!'});
 });
+
+// future routes for the API will be here
+
+// REGISTER ROUTES --------------------------
+// all routes will be prefixed with /api
+
+app.use('/api', router);
+
+// START THE SERVER
+// ================================================================================
+
+app.listen(port);
+console.log('server is listening on port ' + port);
+
+
